d9e5d68e323321f2261e16b10729455f94d82e5c
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-02-15 15:13:11 +0900

    refactor: 型定義ファイル作成し、リファクタ

--- app/routes/api.chat-messages.tsx
+++ app/routes/api.chat-messages.tsx
@@ -1,21 +1,20 @@
-
 export const loader = async (): Promise<any> => {
-  const token = process.env.DIFY_TOKEN
+  const token = process.env.DIFY_TOKEN;
 
-  const response = await fetch('https://api.dify.ai/v1/chat-messages', {
-    method: 'POST',
+  const response = await fetch("https://api.dify.ai/v1/chat-messages", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
-        "inputs": {},
-        "query": "こんにちは",
-        "response_mode": "streaming",
-        "conversation_id": "",
-        "user": "abc-123",
-        "files": []
-    })
+      inputs: {},
+      query: "200文字くらいのダミーテキストを、改行を使いながら日本語で返して",
+      response_mode: "streaming",
+      conversation_id: "",
+      user: "abc-123",
+      files: [],
+    }),
   });
-  return response
-}
+  return response;
+};
--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,35 +1,36 @@
 import { useState } from "react";
+import { parseChunkText } from "~/streamParser";
+import { ChunkChatCompletionResponse } from "~/types/chatMessagesResponse";
 
-export default function Chat() { 
-  const [text, setText] = useState<string>('')
+export default function Chat() {
+  const [text, setText] = useState<string>("");
 
   const sendMessage = async (): Promise<void> => {
-    const response = await fetch('/api/chat-messages')
+    const response = await fetch("/api/chat-messages");
 
-    const reader = response.body?.pipeThrough(new TextDecoderStream()).getReader()
-    while (true) {
-      const {value, done } = await reader?.read()
+    const reader = response.body
+      ?.pipeThrough(new TextDecoderStream())
+      .getReader();
+    for (;;) {
+      const { value, done } = await reader?.read();
       if (done) break;
-      console.log('Received', value)
-      const raws = value.toString().split('\n').filter(line => line.trim() !== '')
-      for (const raw of raws) {
-        const message = raw.replace(/^data: /, '')
-        const parsedData = JSON.parse(message)
-        console.log({parsedData})
-        if (parsedData.answer) {
-          setText((prevText) => prevText + parsedData.answer)
+      const parsedChunkList =
+        parseChunkText<ChunkChatCompletionResponse>(value);
+      parsedChunkList.forEach((chunk) => {
+        if (chunk.event === "message" && chunk.answer) {
+          setText((prevText) => prevText + chunk.answer);
         }
-      }
+      });
     }
-    reader?.releaseLock()
-  }
+    reader?.releaseLock();
+  };
 
   return (
     <div>
       <h1>Chat page</h1>
       <hr />
       <button onClick={sendMessage}>Send Message</button>
-      <p>{ text }</p>
+      <p>{text}</p>
     </div>
-  )
+  );
 }
