d9f999dd17115d13b5423e8e6e0de47e82cf672f
negi524 <egtnrumpbiizarre.info@gmail.com>
2025-02-16 14:06:19 +0900

    feat: yarn add react-markdown

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,9 +1,62 @@
 import { useState } from "react";
 import { DifyStreamParser } from "~/difyStreamParser";
 import { ChunkChatCompletionResponse } from "~/types/chatMessagesResponse";
+import ReactMarkdown from "react-markdown";
 
 export default function Chat() {
   const [text, setText] = useState<string>("");
+  const sampleContent = `
+# h1
+## h2
+### h3
+#### h4
+##### h5
+###### h6
+
+普通のテキスト
+
+- リスト1
+- リスト2
+  - サブリスト1
+
+1. 番号付きリスト1
+2. 番号付きリスト2
+3. 番号付きリスト3
+
+**太字**
+*斜体*
+~~取り消し線~~
+
+[Link](/test)
+
+> 引用1
+
+---
+
+- [ ] 未完了のタスク
+- [x] 完了したタスク
+
+| 見出し1 | 見出し2 |
+|---------|---------|
+| 内容1   | 内容2   |
+| 内容3   | 内容4   |
+
+Markdown:  
+  軽量なマークアップ言語  
+React:  
+  JavaScriptライブラリ
+
+<span style="color: red;">赤い文字</span>
+ 
+[^1]: 注釈です。
+
+<details>
+  <summary>クリックして詳細を見る</summary>
+  詳細な説明がここに入ります。
+</details>
+
+
+  `;
 
   const sendMessage = async (): Promise<void> => {
     const messageBody: ChatMessagesRequestBody = {
@@ -50,6 +103,7 @@ export default function Chat() {
       <hr />
       <button onClick={sendMessage}>Send Message</button>
       <p>{text}</p>
+      <ReactMarkdown>{sampleContent}</ReactMarkdown>
     </div>
   );
 }
